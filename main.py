d7b98c810cea34084591234962381a526edf7096
kaichen12 <160604150+kaichen12@users.noreply.github.com>
2024-03-18 13:14:55 +0000

    final submission

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 stdform = input('Enter a number in scientific notation: ')
-stdform = stdform.strip()
-
 # Type your code below
+stdform = stdform.strip(' ')
+stdform1 = stdform.replace('.','')
+stdform2 = stdform1.replace('-','')
+pos_ = stdform2.find('x')
+pos = stdform2.find('^')
+if not stdform2[:pos_].isdigit():
+    print('Error converting to scientific E notation.')
+elif not stdform2[pos+1:].isdigit():
+    print('Error converting to scientific E notation.')
+elif not stdform.count('.') < 2:
+    print('Error converting to scientific E notation.')  
+elif not stdform.count('x') < 2:
+    print('Error converting to scientific E notation.')
+elif not stdform.count('^') < 2:
+    print('Error converting to scientific E notation.')
+else:
+    stdform1 = stdform.replace('x10^','E')
+    string = f'This number in E notation is {stdform1}.'
+    print(string)
+
+
